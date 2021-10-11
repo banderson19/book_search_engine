87444cdef76b5e00c180c7c8d2a5258f96c8194a
ogMav8 <brad.anderson019@gmail.com>
2021-10-11 10:12:29 -0600

    commiting

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,14 @@
+
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
+mongoose.set('debug', true);
 
 module.exports = mongoose.connection;
